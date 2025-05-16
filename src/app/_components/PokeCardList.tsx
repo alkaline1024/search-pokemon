684735dd4b5b9035630af4af8429b82ef122abcf
alkaline1024 <earthza991@hotmail.com>
2025-05-16 18:08:00 +0700

    perf: ⚡️ image lazy loading

--- src/app/_components/PokeCardList.tsx
+++ src/app/_components/PokeCardList.tsx
@@ -14,34 +14,42 @@ export default function PokeCardList({
     <div
       className={`grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 ${className}`}
     >
-      {pokemons.map((pokemon) => (
-        <div key={pokemon.id}>
-          <div className="flex flex-col gap-4 rounded-lg border border-gray-100 bg-white p-4 shadow">
-            <Image
-              src={pokemon.image}
-              alt={pokemon.name}
-              width={128}
-              height={128}
-              className="h-32 w-full rounded-lg border bg-white object-contain"
-              priority={true}
-            />
-            <div className="text-center">
-              <div className="text-sm opacity-60">#{pokemon.number}</div>
-              <div className="text-lg font-bold">{pokemon.name}</div>
-              <div className="mt-1 flex flex-wrap justify-center gap-1">
-                {pokemon.types.map((type) => (
-                  <span
-                    key={type}
-                    className={`type type-${type.toLowerCase()} `}
-                  >
-                    {type}
-                  </span>
-                ))}
+      {pokemons.map((pokemon) => {
+        return (
+          <div key={pokemon.id}>
+            <div
+              id={pokemon.id}
+              className="flex flex-col gap-4 rounded-lg border border-gray-100 bg-white p-4 shadow"
+            >
+              <Image
+                id={`${pokemon.id}-image`}
+                src={pokemon.image}
+                alt={pokemon.name}
+                unoptimized
+                width={128}
+                height={128}
+                loading="lazy"
+                priority={false}
+                className="h-32 w-full object-contain object-center"
+              />
+              <div className="text-center">
+                <div className="text-sm opacity-60">#{pokemon.number}</div>
+                <div className="text-lg font-bold">{pokemon.name}</div>
+                <div className="mt-1 flex flex-wrap justify-center gap-1">
+                  {pokemon.types.map((type) => (
+                    <span
+                      key={type}
+                      className={`type type-${type.toLowerCase()} `}
+                    >
+                      {type}
+                    </span>
+                  ))}
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 }
