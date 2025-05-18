8f0b06e085ba2ecc81b94215ab099b9d65dfe1f5
alkaline1024 <earthza991@hotmail.com>
2025-05-18 17:49:05 +0700

    feat: 🎸 create search bar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,8 @@ export default function Home() {
   const [pokemons, setPokemons] = useState<IPokemon[]>([]);
   const [loading, setLoading] = useState(true);
   const [hasMore, setHasMore] = useState(true);
+  const [searchText, setSearchText] = useState("");
+  const hasSearchText = searchText.length > 0;
 
   // Constants
   const INIT_FIRST = 20;
@@ -96,6 +98,44 @@ export default function Home() {
 
   return (
     <div className="space-y-6 py-6">
+      <div className="group w-1/2 rounded-full shadow-sm transition-shadow focus-within:!shadow-lg">
+        <div className="relative">
+          <span
+            className={`${hasSearchText ? "scale-0 !opacity-0" : "scale-100"} material-icons absolute top-1/2 left-4 origin-left -translate-y-1/2 opacity-60 transition-all duration-300 group-focus-within:text-gray-800 group-focus-within:opacity-100`}
+          >
+            search
+          </span>
+          <input
+            type="text"
+            className={`${hasSearchText ? "pr-24" : "pl-12"} w-full rounded-full bg-white px-6 py-4 !outline-0 duration-300`}
+            placeholder="Search Pokémon..."
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+          <div
+            className={`${hasSearchText ? "scale-100 opacity-100" : "scale-0 opacity-0"} absolute top-1/2 right-4 flex origin-right -translate-y-1/2 gap-1 transition-all`}
+          >
+            <span
+              className="material-icons cursor-pointer rounded-full p-2 hover:bg-gray-200"
+              onClick={() => {
+                setSearchText("");
+                fetchPokemons(INIT_FIRST);
+              }}
+            >
+              close
+            </span>
+            <span className="my-2 h-auto w-[1px] bg-gray-200" />
+            <span
+              className="material-icons cursor-pointer rounded-full p-2 hover:bg-gray-200"
+              onClick={() => {
+                fetchPokemons(INIT_FIRST);
+              }}
+            >
+              search
+            </span>
+          </div>
+        </div>
+      </div>
       <div
         className="flex flex-col"
         style={{
