ea8b34a347b1ba17d1d250528077b42c97e26b00
alkaline1024 <earthza991@hotmail.com>
2025-05-18 16:27:21 +0700

    feat: 🎸 update pokemon interface by api definitions

--- src/types/pokemon.ts
+++ src/types/pokemon.ts
@@ -2,6 +2,29 @@ declare global {
   type IPokemon = import("@/types/pokemon").IPokemon;
 }
 
+export interface PokemonDimension {
+  minimum: string;
+  maximum: string;
+}
+
+export interface PokemonAttack {
+  fast: Array<{
+    name: string;
+    type: string;
+    damage: number;
+  }>;
+  special: Array<{
+    name: string;
+    type: string;
+    damage: number;
+  }>;
+}
+
+export interface PokemonEvolutionRequirement {
+  amount: number;
+  name: string;
+}
+
 export interface IPokemon {
   id: string;
   number: string;
@@ -11,9 +34,12 @@ export interface IPokemon {
   types: string[];
   resistant: string[];
   weaknesses: string[];
-  weight: { minimum: string; maximum: string };
-  height: { minimum: string; maximum: string };
+  weight: PokemonDimension;
+  height: PokemonDimension;
   fleeRate: number;
   maxCP: number;
   maxHP: number;
+  attacks: PokemonAttack;
+  evolutions?: IPokemon[];
+  evolutionRequirements?: PokemonEvolutionRequirement;
 }
